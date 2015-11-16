f840c55d8ff71cd1e4d1d8b5c8c0c9fa7aba4aed
Jason Mathias <jason.mathias@gmail.com>
2015-11-15 22:41:39 -0800

    Initial Commit

---
+++ Assets/Scripts/GunHighlighter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GunHighlighter : MonoBehaviour {
+
+	public Image handgun;
+	public Image SMG;
+	public Image AK47;
+	public Image RPG;
+
+	public Image[] gunArray;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
