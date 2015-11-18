0b828f6334e856fac9eabb15f74ab154ad637619
Charlie Trinh <charlie@game-school.com>
2015-11-18 13:01:25 -0800

    removed code from the List Script for the Exercise

---
+++ Assets/Scripts/Lists/GunBag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+//Add the correct Collections library
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GunBag : MonoBehaviour {
+	
+	public Image handgun;
+	public Image SMG;
+	public Image AK47;
+	public Image RPG;
+	public int currentImage;
+
+	// create 2 text objects to show the number of guns
+	public Text numGuns;
+	public Text nameGun;
+
+	//Create a new list of images
+
+	// Use this for initialization
+	void Start () {
+		//create a new List of Images
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Add a function for each button of buying a gun
+
+	//Remove the first gun you purchased
+
+
+}
